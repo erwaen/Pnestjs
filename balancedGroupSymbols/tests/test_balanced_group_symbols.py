fd1ded6efc10d6105b80eb156d40da0042ad1ca1
erik <erikwasmosy98@gmail.com>
2023-09-09 12:32:05 -0400

    write more tests

--- balancedGroupSymbols/tests/test_balanced_group_symbols.py
+++ balancedGroupSymbols/tests/test_balanced_group_symbols.py
@@ -1,6 +1,18 @@
 
-from test_balanced_group_symbols import balanced_group_symbols
+from balanced_group_symbols import balanced_group_symbols
 
 def test_balanced_group_symbols():
     assert balanced_group_symbols('[()]{}{()()}') == True
+    assert balanced_group_symbols('()') == True
+    assert balanced_group_symbols('({[]})') == True
+    assert balanced_group_symbols('{{[()]}}') == True
+    assert balanced_group_symbols('(()[{}])') == True
+    assert balanced_group_symbols('()[]{}') == True
+    assert balanced_group_symbols('(()()())') == True
+
     assert balanced_group_symbols('[(])') == False
+    assert balanced_group_symbols('(') == False
+    assert balanced_group_symbols(')') == False
+    assert balanced_group_symbols('({)}') == False
+    assert balanced_group_symbols('{{[()]}}]') == False
+    assert balanced_group_symbols('()[]{}(') == False
